6a7e5748ac58957c18597c32cd53f4686e953392
João Lucas Abreu <joaolucasabreunogueira@gmail.com>
2023-09-26 21:04:06 -0300

    init commit

---
+++ Exercicio 3/Program.cs
@@ -0,0 +1,30 @@
+﻿using SkiaSharp;
+
+namespace Projeto {
+	class Program {
+			static void Main(string[] args) {
+			using (SKBitmap bitmapEntrada = SKBitmap.Decode("Exercicio 3.png")) {
+
+				unsafe {
+					byte* entrada = (byte*)bitmapEntrada.GetPixels();
+					
+					int terra = 0;
+					int agua = 0;
+
+					for (int y = 0; y < bitmapEntrada.Height; y++){
+						for (int x = 0; x < bitmapEntrada.Width; x++){
+							
+							if (entrada[y * bitmapEntrada.Width + x] >= 127)
+								terra++;
+							else
+								agua++;
+						}
+					}
+					
+					Console.WriteLine($"M2 de água = {agua}\nM2 de terra = {terra}");
+				}	
+			}
+
+		}
+	}
+}
---
+++ Exercicio 4/Program.cs
@@ -0,0 +1,26 @@
+﻿using SkiaSharp;
+
+namespace Projeto {
+	class Program {
+		static void Main(string[] args) {
+			// ****************************************************************************
+			// Esse código é só um começo! Você pode precisar de mais imagens de entrada,
+			// ou mais imagens de saída, dependendo do exercício!
+			// ****************************************************************************
+
+			using (SKBitmap bitmap = SKBitmap.Decode("Caminho da imagem de entrada")) {
+				Console.WriteLine(bitmap.ColorType);
+
+				unsafe {
+
+					byte* ptr = (byte*)bitmap.GetPixels();
+
+				}
+
+				using (FileStream stream = new FileStream("Caminho da imagem de saída", FileMode.OpenOrCreate, FileAccess.Write)) {
+					bitmap.Encode(stream, SKEncodedImageFormat.Png, 100);
+				}
+			}
+		}
+	}
+}
