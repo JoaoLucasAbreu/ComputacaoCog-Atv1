2d9a09519967714116e2b684c3f8dc393177fc0b
João Lucas Abreu <joaolucasabreunogueira@gmail.com>
2023-09-27 15:43:06 -0300

    Exercicio 4 completo

--- Exercicio 4/Program.cs
+++ Exercicio 4/Program.cs
@@ -3,22 +3,25 @@
 namespace Projeto {
 	class Program {
 		static void Main(string[] args) {
-			// ****************************************************************************
-			// Esse código é só um começo! Você pode precisar de mais imagens de entrada,
-			// ou mais imagens de saída, dependendo do exercício!
-			// ****************************************************************************
 
-			using (SKBitmap bitmap = SKBitmap.Decode("Caminho da imagem de entrada")) {
-				Console.WriteLine(bitmap.ColorType);
-
+			using (SKBitmap bitmapEntrada = SKBitmap.Decode("Exercicio 4.png"), 
+				bitmapSaida = new SKBitmap(new SKImageInfo(bitmapEntrada.Width, bitmapEntrada.Height, SKColorType.Gray8))) {
+				
 				unsafe {
+					byte* entrada = (byte*)bitmapEntrada.GetPixels();					
+					byte* saida = (byte*)bitmapSaida.GetPixels();
+					int p = 0;
 
-					byte* ptr = (byte*)bitmap.GetPixels();
-
+					for (int y = 0; y < bitmapEntrada.Height; y++){
+						int e = (y + 1) * bitmapEntrada.Width * 4;
+						for (int x = 0; x < bitmapEntrada.Width; x++, p+=4){
+							saida[y * bitmapEntrada.Width + x + (bitmapEntrada.Width - (2 * x))] = (byte)((entrada[p + 2] + entrada[p + 1] + entrada[p] ) / 4);
+						}
+					}
 				}
 
-				using (FileStream stream = new FileStream("Caminho da imagem de saída", FileMode.OpenOrCreate, FileAccess.Write)) {
-					bitmap.Encode(stream, SKEncodedImageFormat.Png, 100);
+				using (FileStream stream = new FileStream("Exercicio 4 reverse.png", FileMode.OpenOrCreate, FileAccess.Write)) {
+					bitmapSaida.Encode(stream, SKEncodedImageFormat.Png, 100);
 				}
 			}
 		}
